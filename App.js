33a912cd34fb115596692887a19b9698ba566aae
cleverTon <35335846+desenvolvedorIcotrade@users.noreply.github.com>
2018-09-25 01:39:57 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './logo.svg';
+class App extends Component {
+    render() {
+        return ( < div className = "App" >
+            <
+            header className = "App-header" >
+            <
+            img src = {
+                logo
+            }
+            className = "App-logo"
+            alt = "logo" / >
+            <
+            h1 className = "App-title" > Otimize adicione um Todo < /h1> </header >
+            <
+            p className = "App-intro" >
+            <
+            code > xx - inspecionar - Console - xx < /code>  < /p >
+            <
+            /div>
+        );
+    }
+}
+
+export default App
